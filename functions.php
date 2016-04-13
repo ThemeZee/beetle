078cbe30aec40d698186ef14950a092f59173ad1
Thomas W <netzberufler@googlemail.com>
2016-04-13 15:33:27 +0200

    completed custom logo feature

--- functions.php
+++ functions.php
@@ -51,8 +51,8 @@ function beetle_setup() {
 	
 	// Set up the WordPress core custom logo feature
 	add_theme_support( 'custom-logo', apply_filters( 'beetle_custom_logo_args', array(
-		'height' => 50,
-		'width' => 350,
+		'height' => 40,
+		'width' => 250,
 		'flex-height' => true,
 		'flex-width' => true,
 	) ) );
@@ -95,7 +95,7 @@ function beetle_widgets_init() {
 	register_sidebar( array(
 		'name' => esc_html__( 'Sidebar', 'beetle' ),
 		'id' => 'sidebar',
-		'description' => esc_html__( 'Appears on posts and pages except full width template.', 'beetle' ),
+		'description' => esc_html__( 'Appears on posts and pages except the full width template.', 'beetle' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s clearfix">',
 		'after_widget' => '</aside>',
 		'before_title' => '<div class="widget-header"><h3 class="widget-title">',
