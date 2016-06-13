de721055bc0b87bdb1c737be25f53218352955d5
Thomas W <netzberufler@googlemail.com>
2016-06-13 11:47:19 +0200

    improved code formatting for Customizer files

--- inc/customizer/default-options.php
+++ inc/customizer/default-options.php
@@ -7,28 +7,26 @@
  * @package Beetle
  */
 
-
 /**
  * Get saved user settings from database or theme defaults
  *
  * @return array
  */
 function beetle_theme_options() {
-    
-	// Merge Theme Options Array from Database with Default Options Array
-	$theme_options = wp_parse_args( 
-		
-		// Get saved theme options from WP database
-		get_option( 'beetle_theme_options', array() ), 
-		
-		// Merge with Default Options if setting was not saved yet
-		beetle_default_options() 
-		
+
+	// Merge Theme Options Array from Database with Default Options Array.
+	$theme_options = wp_parse_args(
+
+		// Get saved theme options from WP database.
+		get_option( 'beetle_theme_options', array() ),
+
+		// Merge with Default Options if setting was not saved yet.
+		beetle_default_options()
 	);
 
-	// Return theme options
+	// Return theme options.
 	return $theme_options;
-	
+
 }
 
 
@@ -61,6 +59,6 @@ function beetle_default_options() {
 		'slider_animation' 					=> 'slide',
 		'slider_speed' 						=> 7000,
 	);
-	
+
 	return $default_options;
 }
